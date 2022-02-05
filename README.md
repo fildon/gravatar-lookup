805c92afdae5fadbc6f96aeabe003e94bffbf676
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-02-05 07:04:51 +0100

    💯 MVP looking good

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # gravatar-lookup
+
+A web interface to lookup a Gravatar. Currently deployed here: [Gravatar Lookup](https://fildon.me/gravatar-lookup)
+
+## Developer Guidance
+
+1. Install dependencies
+
+   ```shell
+   npm install
+   ```
+
+2. Build the app
+
+   ```shell
+   npm run build:dev
+   ```
+
+3. Serve locally
+
+   ```shell
+   npm run serve
+   ```
+
+You will now have a running application on localhost.
+
+## Deployment
+
+Deployment is handled automatically via Github Actions.
+
+Any update to the `main` branch will trigger a new build and deploy.
+
+## Contributing
+
+Contributions are always welcome!
+
+Please begin by raising or commenting on a GitHub issue.
+Then feel free to open a PR.
---
+++ src/app.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import md5 from "md5";
+
+const App = () => {
+  const [inputValue, setInputValue] = React.useState("");
+  const [submittedValue, setSubmittedValue] = React.useState("");
+  const onChange = (event: { target: { value: string } }) => {
+    setInputValue(event.target.value);
+  };
+  const onSubmit = (event: { preventDefault: () => unknown }) => {
+    event.preventDefault();
+    setSubmittedValue(inputValue.trim().toLowerCase());
+  };
+  return (
+    <>
+      <h1>Gravatar Lookup</h1>
+      <p>
+        Enter an email address below to perform a{" "}
+        <a href="https://en.gravatar.com/">Gravatar</a> lookup
+      </p>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={inputValue} onChange={onChange}></input>
+        <input type="submit" value="Search!" />
+      </form>
+      <img
+        src={`https://www.gravatar.com/avatar/${md5(
+          submittedValue
+        )}?s=200&d=identicon`}
+        alt={`Gravatar for ${submittedValue}`}
+      />
+    </>
+  );
+};
+
+render(<App />, document.getElementById("root"));
